82fa0e0435a7c3a5e0883524dc95f1cdc2d0d6da
Paul Kepley <pakepley@gmail.com>
2017-07-29 17:32:48 -0500

    Demo draw feature

---
+++ demo_wiggle_draw.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from wiggle_artist import wiggle_artist
+
+# Plot Cage
+image_file = './imgs/NickCage.jpg'
+wa = wiggle_artist(image_file = image_file, 
+				   block_height = 4, 
+				   line_color = (0,0,1,0.5), 
+				   bg_color='w')
+wa.draw()
+wa.fig.set_size_inches(12,12)
+plt.savefig('./imgs/NickCage_{0}.png'.format(wa.block_height),
+			bbox_inches='tight',
+			dpi=100)
+
+# Plot Lena
+image_file = './imgs/Lena.jpg'
+wa = wiggle_artist(image_file = image_file, 
+				   block_height = 8, 
+				   line_color = (0,0,1,0.5), 
+				   bg_color='w')
+wa.draw()
+wa.fig.set_size_inches(12,12)
+plt.savefig('./imgs/Lena_{0}.png'.format(wa.block_height),
+			bbox_inches='tight',
+			dpi=100)
+
+# Show results
+plt.show()

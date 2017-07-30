52075f86f3d615790d4ff1728738b52eaafc0c62
Paul Kepley <pakepley@gmail.com>
2017-07-30 16:02:10 -0500

    Use 4 spaces in place of tabs

--- wave_animator.py
+++ wave_animator.py
@@ -3,55 +3,55 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 
 class wave_animator:
-	def __init__(self, solver, fig, ax, lw = 2, color='b', steps_per_frame = 1):
-		self.solver = solver		
-		self.fig = fig
-		self.ax = ax
-		self.lines = []
-		self.lw = lw
-		self.color = color
-		self.steps_per_frame = steps_per_frame
+    def __init__(self, solver, fig, ax, lw = 2, color='b', steps_per_frame = 1):
+        self.solver = solver        
+        self.fig = fig
+        self.ax = ax
+        self.lines = []
+        self.lw = lw
+        self.color = color
+        self.steps_per_frame = steps_per_frame
 
-		for i in range(solver.nr):
-			lobj = self.ax.plot([],[],lw = self.lw,color = self.color)[0]
-			self.lines.append(lobj)
-		
-	def init(self):
-		for line in self.lines:
-			line.set_data([],[])
-		return self.lines
+        for i in range(solver.nr):
+            lobj = self.ax.plot([],[],lw = self.lw,color = self.color)[0]
+            self.lines.append(lobj)
+        
+    def init(self):
+        for line in self.lines:
+            line.set_data([],[])
+        return self.lines
 
-	def animate(self,i):
-		for j in range(self.steps_per_frame):
-			self.solver.step()
-		for j, line in enumerate(self.lines):
-			line.set_data(self.solver.x, self.solver.w_cur[j,:])
-		return self.lines
+    def animate(self,i):
+        for j in range(self.steps_per_frame):
+            self.solver.step()
+        for j, line in enumerate(self.lines):
+            line.set_data(self.solver.x, self.solver.w_cur[j,:])
+        return self.lines
 
 
 if __name__ == '__main__':
-	from dirichlet_wave_solver import dirichlet_wave_solver
-	nx = 1000
-	nt = 1000
-	x = np.linspace(0,2,1000)
-	w0 = []
-	mm = 60
+    from dirichlet_wave_solver import dirichlet_wave_solver
+    nx = 1000
+    nt = 1000
+    x = np.linspace(0,2,1000)
+    w0 = []
+    mm = 60
 
-	fig = plt.figure()
-	ax = plt.axes(xlim=(x[0], x[-1]), ylim=(-3, 3))
+    fig = plt.figure()
+    ax = plt.axes(xlim=(x[0], x[-1]), ylim=(-3, 3))
 
-	for i in range(mm):
-		w0.append(np.array([np.exp(-(x-((i+1)*(1.0/mm)))**2 / .1**2)+(-2+ (3.0*i)/mm)]).flatten())
-	w0 = np.array(w0)
+    for i in range(mm):
+        w0.append(np.array([np.exp(-(x-((i+1)*(1.0/mm)))**2 / .1**2)+(-2+ (3.0*i)/mm)]).flatten())
+    w0 = np.array(w0)
 
-	solver = dirichlet_wave_solver(x,w0, np.zeros(w0.shape), 
-								dt = x[1] - x[0], 
-								dx = x[1] - x[0])
+    solver = dirichlet_wave_solver(x,w0, np.zeros(w0.shape), 
+                                dt = x[1] - x[0], 
+                                dx = x[1] - x[0])
 
-	wa = wave_animator(solver, fig, ax, steps_per_frame = 20)
+    wa = wave_animator(solver, fig, ax, steps_per_frame = 20)
 
-	anim = animation.FuncAnimation(wa.fig, wa.animate, init_func=wa.init,
-								   frames=2, interval=2, blit=True)
+    anim = animation.FuncAnimation(wa.fig, wa.animate, init_func=wa.init,
+                                   frames=2, interval=2, blit=True)
 
 
-	plt.show()
+    plt.show()
--- wiggle_artist.py
+++ wiggle_artist.py
@@ -17,71 +17,71 @@ def moving_average(a, n=3) :
 ## wiggle_artist
 #####################################################################################
 class wiggle_artist:
-	def __init__(self, image_file, block_height, bg_color = 'b', line_color = 'w'):
-		self.im = imread(image_file, flatten=True)		
-		self.nr, self.nc = self.im.shape
+    def __init__(self, image_file, block_height, bg_color = 'b', line_color = 'w'):
+        self.im = imread(image_file, flatten=True)        
+        self.nr, self.nc = self.im.shape
 
-		self.block_height = block_height
-		self.n_block_rows = int(np.ceil(self.nr / self.block_height))
-		self.n_block_cols = self.nc - int(np.ceil(self.block_height / 2.0)) + 1
+        self.block_height = block_height
+        self.n_block_rows = int(np.ceil(self.nr / self.block_height))
+        self.n_block_cols = self.nc - int(np.ceil(self.block_height / 2.0)) + 1
 
-		self.line_width = np.sqrt(block_height)
-		self.line_color = line_color
-		self.bg_color = bg_color
+        self.line_width = np.sqrt(block_height)
+        self.line_color = line_color
+        self.bg_color = bg_color
 
-		self.sigma = np.sqrt(np.var(self.im))
+        self.sigma = np.sqrt(np.var(self.im))
 
-		self.wiggles = np.zeros((self.n_block_rows, self.n_block_cols))
+        self.wiggles = np.zeros((self.n_block_rows, self.n_block_cols))
 
-		for i in range(0, self.n_block_rows):
-			row = i * self.block_height
-			row_height = self.nr - row
-			mu = np.mean(self.im[row,:])
-			self.wiggles[i,:] = moving_average(self.im[row,:], int(np.ceil(self.block_height / 2.0)))
-			self.wiggles[i,:] = row_height + np.sqrt(self.block_height) * ((self.wiggles[i,:] - mu) / self.sigma)
-		self.fig, self.ax = plt.subplots()
-		self.ax.axis([self.block_height, self.nc-self.block_height, 
-					  self.block_height, self.nr-self.block_height])
-		self.ax.set_xticks([])
-		self.ax.set_yticks([])
-		self.ax.patch.set_facecolor(self.bg_color)
+        for i in range(0, self.n_block_rows):
+            row = i * self.block_height
+            row_height = self.nr - row
+            mu = np.mean(self.im[row,:])
+            self.wiggles[i,:] = moving_average(self.im[row,:], int(np.ceil(self.block_height / 2.0)))
+            self.wiggles[i,:] = row_height + np.sqrt(self.block_height) * ((self.wiggles[i,:] - mu) / self.sigma)
+        self.fig, self.ax = plt.subplots()
+        self.ax.axis([self.block_height, self.nc-self.block_height, 
+                      self.block_height, self.nr-self.block_height])
+        self.ax.set_xticks([])
+        self.ax.set_yticks([])
+        self.ax.patch.set_facecolor(self.bg_color)
 
-	def draw(self):
-		for i in range(0, self.n_block_rows):
-			self.ax.plot(self.wiggles[i,:], c = self.line_color, lw = self.line_width)
-		return self.fig, self.ax
+    def draw(self):
+        for i in range(0, self.n_block_rows):
+            self.ax.plot(self.wiggles[i,:], c = self.line_color, lw = self.line_width)
+        return self.fig, self.ax
 
-	def init_wiggler(self, solver_class, steps_per_frame = 1):
-		self.x = np.linspace(0, self.n_block_cols, self.n_block_cols)
-		self.w0 = self.wiggles
-		self.v0 = np.zeros(self.w0.shape)
-		self.dt = self.x[1] - self.x[0]
-		self.dx = self.x[1] - self.x[0]
-		self.steps_per_frame = steps_per_frame
+    def init_wiggler(self, solver_class, steps_per_frame = 1):
+        self.x = np.linspace(0, self.n_block_cols, self.n_block_cols)
+        self.w0 = self.wiggles
+        self.v0 = np.zeros(self.w0.shape)
+        self.dt = self.x[1] - self.x[0]
+        self.dx = self.x[1] - self.x[0]
+        self.steps_per_frame = steps_per_frame
 
-		self.solver = solver_class(self.x, self.w0, self.v0, self.dt, self.dx)
-		self.waver = wave_animator(self.solver, self.fig, self.ax, self.line_width, self.line_color,
-								   self.steps_per_frame)
-		
+        self.solver = solver_class(self.x, self.w0, self.v0, self.dt, self.dx)
+        self.waver = wave_animator(self.solver, self.fig, self.ax, self.line_width, self.line_color,
+                                   self.steps_per_frame)
+        
 
 if __name__ == '__main__':
-	from dirichlet_wave_solver import dirichlet_wave_solver
+    from dirichlet_wave_solver import dirichlet_wave_solver
 
-	# Plot Cage
-	image_file = './imgs/NickCage.jpg'
-	wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
-	wa.draw()
+    # Plot Cage
+    image_file = './imgs/NickCage.jpg'
+    wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
+    wa.draw()
 
-	# Plot Cage
-	image_file = './imgs/NickCage.jpg'
-	wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
-	wa.draw()
+    # Plot Cage
+    image_file = './imgs/NickCage.jpg'
+    wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
+    wa.draw()
 
-	
-	# # Plot Lena
-	# image_file = './imgs/Lena.jpg'
-	# wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
-	# wa.init_wiggler(dirichlet_wave_solver)
-	# anim = animation.FuncAnimation(wa.fig, wa.waver.animate, init_func = wa.waver.init,
-	# 							   frames=25, interval=0.1, blit=True)
-	plt.show()
+    
+    # # Plot Lena
+    # image_file = './imgs/Lena.jpg'
+    # wa = wiggle_artist(image_file, block_height = 4, line_color= (0,0,1,0.5), bg_color='w')
+    # wa.init_wiggler(dirichlet_wave_solver)
+    # anim = animation.FuncAnimation(wa.fig, wa.waver.animate, init_func = wa.waver.init,
+    #                                frames=25, interval=0.1, blit=True)
+    plt.show()
